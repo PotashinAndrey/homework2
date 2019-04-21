219ce49472ab764df9ccb5bb2e562350331a8640
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2019-04-21 14:26:19 +0500

    Окончательное выполнение основных требований д/з №2

--- src/index.js
+++ src/index.js
@@ -1,67 +1,75 @@
 import "babel-polyfill";
 import Chart from "chart.js";
 
-const currencyURL = "www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
-// const meteoURL = "/xml.meteoservice.ru/export/gismeteo/point/140.xml";
+const meteoURL = "/xml.meteoservice.ru/export/gismeteo/point/140.xml";
 
-async function loadCurrency() {
-  const response = await fetch(currencyURL);
+async function loadTemperature() {
+  const response = await fetch(meteoURL);
   const xmlTest = await response.text();
   const parser = new DOMParser();
-  const currencyData = parser.parseFromString(xmlTest, "text/xml");
-  // <Cube currency="USD" rate="1.1321" />
-  const rates = currencyData.querySelectorAll("Cube[currency][rate]");
-  const result = Object.create(null);
-  for (let i = 0; i < rates.length; i++) {
-    const rateTag = rates.item(i);
-    const rate = rateTag.getAttribute("rate");
-    const currency = rateTag.getAttribute("currency");
-    result[currency] = rate;
+  const weatherData = parser.parseFromString(xmlTest, "text/xml");
+  const time = weatherData.querySelectorAll("FORECAST");
+  const tempRange = weatherData.querySelectorAll("TEMPERATURE");
+  const timeDt = [];
+  const weatherDataMax = [];
+  const weatherDataMin = [];
+  for (let i = 0; i < time.length; i++) {
+    const Tag = time.item(i);
+    const day = Tag.getAttribute("day");
+    const month = Tag.getAttribute("month");
+    timeDt[i] = String(day) + '.' + String(month);
   }
-  result["EUR"] = 1;
-  // result["RANDOM"] = 1 + Math.random();
-  return result;
-}
-
-function normalizeDataByCurrency(data, currency) {
-  const result = Object.create(null);
-  const value = data[currency];
-  for (const key of Object.keys(data)) {
-    result[key] = value / data[key];
+  for (let i = 0; i < tempRange.length; i++) {
+    const Tag = tempRange.item(i);
+    const minimum = Tag.getAttribute("min");
+    const maximum = Tag.getAttribute("max");
+    weatherDataMax[i] = maximum;
+    weatherDataMin[i] = minimum;
   }
-  return result;
+  return { time: timeDt, temperature: { max: weatherDataMax, min: weatherDataMin } };
 }
 
 const buttonBuild = document.getElementById("btn");
 const canvasCtx = document.getElementById("out").getContext("2d");
-buttonBuild.addEventListener("click", async function() {
-  const currencyData = await loadCurrency();
-  const normalData = normalizeDataByCurrency(currencyData, "RUB");
-  const keys = Object.keys(normalData).sort((k1, k2) =>
-    compare(normalData[k1], normalData[k2])
-  );
-  const plotData = keys.map(key => normalData[key]);
+buttonBuild.addEventListener("click", async function () {
+  const currencyData = await loadTemperature();
 
   const chartConfig = {
     type: "line",
 
     data: {
-      labels: keys,
+      labels: currencyData.time,
       datasets: [
         {
-          label: "Стоимость валюты в рублях",
-          backgroundColor: "rgb(255, 20, 20)",
-          borderColor: "rgb(180, 0, 0)",
-          data: plotData
+          label: "Минимальная темпиратура",
+          borderColor: "rgb(200, 0, 0)",
+          backgroundColor: "rgb(200, 0, 0, 0.05)",
+          data: currencyData.temperature.min
+        },
+        {
+          label: "Максимальная темпиратура",
+          borderColor: "rgb(0, 200, 0)",
+          backgroundColor: "rgb(0, 200, 0, 0.05)",
+          data: currencyData.temperature.max
         }
       ]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            min: Math.min(...currencyData.temperature.min) - delta(currencyData.temperature.min),
+            max: Math.max(...currencyData.temperature.max) + delta(currencyData.temperature.min)
+          }
+        }]
+      }
     }
   };
 
   if (window.chart) {
-    chart.data.labels = chartConfig.data.labels;
-    chart.data.datasets[0].data = chartConfig.data.datasets[0].data;
-    chart.update({
+    window.chart.data.labels = chartConfig.data.labels;
+    window.chart.data.datasets[0].data = chartConfig.data.datasets[0].data;
+    window.chart.update({
       duration: 800,
       easing: "easeOutBounce"
     });
@@ -70,8 +78,6 @@ buttonBuild.addEventListener("click", async function() {
   }
 });
 
-function compare(a, b) {
-  if (a > b) return 1;
-  if (a < b) return -1;
-  return 0;
+function delta(array) {
+  return Math.max(Math.abs(Math.min(...array)), Math.max(...array))/5;
 }
